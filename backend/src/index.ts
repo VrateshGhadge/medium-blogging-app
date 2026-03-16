327be2dcfc31f43bf6a0bbb70c977947746fc9fa
Vratesh <ghadgevratesh@gmail.com>
2026-03-16 23:43:56 +0530

    blogging-app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,16 @@
-import { Hono } from 'hono';
+import { Hono } from 'hono'
+const app = new Hono()
 
-// Create the main Hono app
-const app = new Hono();
+app.post('/api/v1/signup', (c) => c.text('Hono!'))
 
-app.post('/api/v1/signup', (c) => {
-	return c.text('signup route')
-})
+app.post('/api/v1/signin', (c) => c.text('Hono!'))
 
-app.post('/api/v1/signin', (c) => {
-	return c.text('signin route')
-})
+app.post('/api/v1/blog', (c) => c.text('Hono!'))
 
-app.get('/api/v1/blog/:id', (c) => {
-	const id = c.req.param('id')
-	console.log(id);
-	return c.text('get blog route')
-})
+app.put('/api/v1/blog', (c) => c.text('Hono!'))
 
-app.post('/api/v1/blog', (c) => {
+app.get('/api/v1/blog/:id', (c) => c.text('Hono!'))
 
-	return c.text('signin route')
-})
+app.get('./api/v1/blog/bulk')
 
-app.put('/api/v1/blog', (c) => {
-	return c.text('signin route')
-})
-
-export default app;
+export default app 
