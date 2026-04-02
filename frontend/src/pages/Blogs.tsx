cd48bbc70192b575c07f7bf6ae906811b4505e47
Vratesh <ghadgevratesh@gmail.com>
2026-04-03 04:03:43 +0530

    added blog-card

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,32 @@
+import { BlogCard } from "../components/BlogCard"
+
+export const Blogs = ()=>{
+    return <div className="flex justify-center">
+        <div className="max-w-xl">
+            <BlogCard
+                authorName={"Joe jones"}
+                title={"How an ugly website make $5000 a month without any adverstising and affiliate marketing"}
+                content={"How an ugly website make $5000 a month without any adverstising and affiliate marketing How an ugly website make $5000 a month without any adverstising and affiliate marketing How an ugly website make $5000 a month without any adverstising and affiliate marketing "}
+                publishedDate={"3rd April 26"}
+            />
+            <BlogCard
+                authorName={"Joe jones"}
+                title={"How an ugly website make $5000 a month without any adverstising and affiliate marketing"}
+                content={"How an ugly website make $5000 a month without any adverstising and affiliate marketing How an ugly website make $5000 a month without any adverstising and affiliate marketing How an ugly website make $5000 a month without any adverstising and affiliate marketing "}
+                publishedDate={"3rd April 26"}
+            />
+            <BlogCard
+                authorName={"Joe jones"}
+                title={"How an ugly website make $5000 a month without any adverstising and affiliate marketing"}
+                content={"How an ugly website make $5000 a month without any adverstising and affiliate marketing How an ugly website make $5000 a month without any adverstising and affiliate marketing How an ugly website make $5000 a month without any adverstising and affiliate marketing "}
+                publishedDate={"3rd April 26"}
+            />
+            <BlogCard
+                authorName={"Joe jones"}
+                title={"How an ugly website make $5000 a month without any adverstising and affiliate marketing"}
+                content={"How an ugly website make $5000 a month without any adverstising and affiliate marketing How an ugly website make $5000 a month without any adverstising and affiliate marketing How an ugly website make $5000 a month without any adverstising and affiliate marketing "}
+                publishedDate={"3rd April 26"}
+            />
+        </div>
+    </div>
+}
